08f19a408b888aa790cf3689fe8bf1c8d9cca325
ndjndj <teq.ndj@gmail.com>
2024-01-06 23:24:49 +0900

    f

--- api/line_v_to_m.py
+++ api/line_v_to_m.py
@@ -9,5 +9,13 @@ import sys
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 LINE_BOT_API = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
+def reply(ev): 
+    reply_token = ev["replyToken"]
+    message = ev["message"]["text"]
+    LINE_BOT_API.reply_message(
+        reply_token, 
+        TextSendMessage(text=message)
+    )
+
 def lambda_handler(event, context):
     return {}
