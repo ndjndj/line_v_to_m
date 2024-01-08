8fecd9a978920a4254d2ba35b752d51169e7c877
ndjndj <teq.ndj@gmail.com>
2024-01-09 01:27:42 +0900

    f

--- api/app.py
+++ api/app.py
@@ -1,17 +1,24 @@
 import os
-#import json
+import json
 #import boto3
 #from boto3.dynamodb.conditions import Key
-#from linebot import LineBotApi
-#from linebot.models import TextSendMessage
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
 import sys
 
-#LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-#LINE_BOT_API = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-#
-#def reply(ev): 
-#    reply_token = ev["replyToken"]
-#    message = ev["message"]["text"]
+IS_TEST = os.environ("IS_TEST")
+
+if IS_TEST == 1:
+    LINE_CHANNEL_ACCESS_TOKEN = ""
+    LINE_BOT_API = ""
+else:
+    LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+    LINE_BOT_API = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+
+def reply(ev): 
+    reply_token = ev["replyToken"]
+    message = ev["message"]["text"]
+
 #    LINE_BOT_API.reply_message(
 #        reply_token, 
 #        TextSendMessage(text=message)
